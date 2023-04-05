9ace4b2e40c2d4852f422577d2126d1784ae760c
Brian Vanderwende <vanderwb@ucar.edu>
2023-04-05 14:27:20 -0600

    Standardize init process

--- share/ncar/init.sh
+++ share/ncar/init.sh
@@ -1,16 +1,17 @@
-# The bashrc.local script will *always* be sourced for interactive
-# bash shells, but not for script shells. Here we ensure consistent
-# behavior.
-if [[ $- != *i* ]]; then
-    if [[ -f /etc/bash.bashrc.local ]]; then
-        . /etc/bash.bashrc.local
-    fi
-fi
+# Make sure module environment is consistent regardless of whether
+# we are working on a clean system or not!
+if [[ -f /etc/bash.bashrc.local ]]; then
+    . /etc/bash.bashrc.local
+elif [[ -f /etc/profile.d/z00_modules.sh ]]; then
+    . /etc/profile.d/z00_modules.sh 2> /dev/null
 
-# If modules are present (Cray), make sure none are loaded
-# (Spack will do this for properly configured compilers/externals)
-if [[ -n $(type -t module) ]]; then
-    module purge &> /dev/null
+    if module --force purge >& /dev/null; then
+        cray_module=$(module -t --show-hidden av crayenv |& tail -n1)
+
+        if [[ -n $cray_module ]]; then
+            module load $cray_module
+        fi
+    fi
 fi
 
 # If left set, will contaminate Spack child shells
